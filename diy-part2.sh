f61197617b71840d2f0c5f1f4111307b8c25d1a7
iFHaxx <anzclan@proton.me>
2025-04-19 11:21:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,16 +1,39 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
 
-#Add new luci-app-openclash
+# Navigate to the package directory
+cd ./openwrt/package
+
+# Add custom packages by cloning the repositories
+git clone --depth=1 https://github.com/immortalwrt/luci luci
+git clone --depth=1 https://github.com/immortalwrt/packages packages
+git clone --depth=1 https://github.com/intannajwa/joss.git joss
+git clone --depth=1 https://github.com/FUjr/modem_feeds.git modem_feeds
+git clone --depth=1 https://github.com/intannajwa/quectel_CM_5G_M.git quectel_CM_5G_M
+git clone --depth=1 https://github.com/solomonricky/luci-app-modeminfo.git luci-app-modeminfo
+git clone --depth=1 https://github.com/4IceG/luci-app-3ginfo-lite.git luci-app-3ginfo-lite
+git clone --depth=1 https://github.com/4IceG/luci-app-atcommands.git luci-app-atcommands
+git clone --depth=1 https://github.com/4IceG/luci-app-modemband.git luci-app-modemband
+git clone --depth=1 https://github.com/4IceG/luci-app-sms-tool-js.git luci-app-sms-tool-js
+git clone --depth=1 https://github.com/gSpotx2f/luci-app-internet-detector.git luci-app-internet-detector
+git clone --depth=1 https://github.com/4IceG/luci-app-irqbalance.git luci-app-irqbalance
+
+# Add new luci-app-openclash
 rm -rf feeds/luci/applications/luci-app-openclash
 git clone -b dev --depth 1 https://github.com/vernesong/OpenClash.git /tmp/OpenClash
 mv /tmp/OpenClash/luci-app-openclash package/
+
+# Navigate to the openwrt directory to update and install feeds
+cd ./openwrt
+
+# Update and install feeds
+./scripts/feeds update -a
+./scripts/feeds install -a
+./scripts/feeds install -a -f -p modem-extras
+./scripts/feeds install -a -f -p modem 
+./scripts/feeds install luci-app-modemband
+./scripts/feeds install luci-app-modeminfo
+./scripts/feeds install -a -f -p irqbalance
+./scripts/feeds install luci-app-irqbalance
+./scripts/feeds install quectel_CM_5G_M
+./scripts/feeds install luci-app-telegrambot
+./scripts/feeds install luci-app-ttl
