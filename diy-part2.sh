449816e31f8812feaf22e27bc0a674a0f6eefdf9
4hidess <107763782+4hidess@users.noreply.github.com>
2025-04-19 15:12:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -3,23 +3,6 @@
 # Navigate to the package directory
 cd package
 
-# Add custom packages by cloning the repositories
-git clone --depth=1 https://github.com/immortalwrt/luci luci
-git clone --depth=1 https://github.com/immortalwrt/packages packages
-git clone --depth=1 https://github.com/intannajwa/joss.git joss
-git clone --depth=1 https://github.com/FUjr/modem_feeds.git modem_feeds
-git clone --depth=1 https://github.com/intannajwa/quectel_CM_5G_M.git quectel_CM_5G_M
-git clone --depth=1 https://github.com/solomonricky/luci-app-modeminfo.git luci-app-modeminfo
-git clone --depth=1 https://github.com/4IceG/luci-app-3ginfo-lite.git luci-app-3ginfo-lite
-git clone --depth=1 https://github.com/4IceG/luci-app-atcommands.git luci-app-atcommands
-git clone --depth=1 https://github.com/4IceG/luci-app-modemband.git luci-app-modemband
-git clone --depth=1 https://github.com/4IceG/luci-app-sms-tool-js.git luci-app-sms-tool-js
-git clone --depth=1 https://github.com/gSpotx2f/luci-app-internet-detector.git luci-app-internet-detector
-git clone --depth=1 https://github.com/4IceG/luci-app-irqbalance.git luci-app-irqbalance
-git clone --depth=1 https://github.com/xx-vv/quectel_cm_5G quectel_cm_5G 
-# Navigate to the openwrt directory to update and install feeds
-cd
-
 # Add new luci-app-openclash
 rm -rf feeds/luci/applications/luci-app-openclash
 git clone -b dev --depth 1 https://github.com/vernesong/OpenClash.git /tmp/OpenClash
